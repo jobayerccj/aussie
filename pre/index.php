11e303fddca4d20f47e8dc662591e3dd1656b910
Md. Jobayer Islam <jobayerccj@gmail.com>
2015-04-06 14:28:39 +0600

    home page html complete

---
+++ pre/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Aussie</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        
+                    <header>
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-offset-2 col-md-2 logo">
+                                    <img src="images/logo.gif" alt="logo"/>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="pull-left">
+                                        <h3>Aussie Immigration & Education Consultancy </h3>
+                                    </div>
+                                    <div class="pull-right">
+                                        <img src="" alt="sc1"/>
+                                        <img src="" alt="sc2"/>
+                                        <img src="" alt="sc3"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </header>
+                
+    </body>
+</html>
